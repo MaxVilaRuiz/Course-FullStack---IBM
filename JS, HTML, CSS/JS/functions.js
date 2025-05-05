4001ebd7c25d28065240db87e88d9879fb36ad88
Max Vilà Ruiz <max16506@gmail.com>
2025-05-05 19:35:13 +0200

    Section 3 - JavaScript

---
+++ JS, HTML, CSS/JS/functions.js
@@ -0,0 +1,25 @@
+// Prototypes / Structs / Classes: 
+function Car(make, model, year) {
+    // 'this' refer to the object that is refered to the function.
+    this.make = make;
+    this.model = model;
+    this.year = year;
+
+    // To create a method for the struct:
+    this.getName = function () {
+        return this.make + ' ' + this.model + ' ' + this.year;
+    }
+}
+
+// Another way to create the method for the prototype 'Car':
+Car.prototype.getName = function() {
+    return this.make + ' ' + this.model + ' ' + this.year;
+}
+
+
+
+// Self-executing functions:
+// Are executed when the program starts running
+(function () {
+    //statements
+})
